bea8cbc8538df68d93f519d6178d62938c00a513
Francisco Sousa <franciscoeds1987@hotmail.com>
2023-09-09 15:26:00 -0300

    criando o adapter de inserção do cliente

---
+++ src/main/java/com/francisco/hexagonal/adapters/out/repository/mapper/CustomerEntityMapper.java
@@ -0,0 +1,13 @@
+package com.francisco.hexagonal.adapters.out.repository.mapper;
+
+import org.mapstruct.Mapper;
+
+import com.francisco.hexagonal.adapters.out.repository.entity.CustomerEntity;
+import com.francisco.hexagonal.application.core.domain.Customer;
+
+@Mapper(componentModel = "spring")
+public interface CustomerEntityMapper {
+	
+	CustomerEntity toCustomerEntity(Customer customer);
+
+}
