b67fb1f5144a9235b5644514bcd0b835f8481048
Francisco Sousa <franciscoeds1987@hotmail.com>
2023-09-11 13:49:47 -0300

    criando o controller CRUD - READ

--- src/main/java/com/francisco/hexagonal/adapters/in/controller/mapper/CustomerMapper.java
+++ src/main/java/com/francisco/hexagonal/adapters/in/controller/mapper/CustomerMapper.java
@@ -4,6 +4,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 import com.francisco.hexagonal.adapters.in.controller.request.CustomerRequest;
+import com.francisco.hexagonal.adapters.in.controller.response.CustomerResponse;
 import com.francisco.hexagonal.application.core.domain.Customer;
 
 @Mapper(componentModel = "spring")
@@ -13,5 +14,7 @@ public interface CustomerMapper {
 	@Mapping(target = "address", ignore = true)
 	@Mapping(target = "isValidCpf", ignore = true)
 	Customer toCustomer(CustomerRequest customerRequest);
+	
+	CustomerResponse toCustomerResponse(Customer customer);
 
 }
