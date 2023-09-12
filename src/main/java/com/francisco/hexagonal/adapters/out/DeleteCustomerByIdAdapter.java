161ecf8706e8ef1f03157ec3e5b8f577d185f300
Francisco Sousa <franciscoeds1987@hotmail.com>
2023-09-12 16:03:30 -0300

    criando o adapter CRUD - DELETE

---
+++ src/main/java/com/francisco/hexagonal/adapters/out/DeleteCustomerByIdAdapter.java
@@ -0,0 +1,20 @@
+package com.francisco.hexagonal.adapters.out;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.francisco.hexagonal.adapters.out.repository.CustomerRepository;
+import com.francisco.hexagonal.application.ports.out.DeleteCustomerByIdOutputPort;
+
+@Component
+public class DeleteCustomerByIdAdapter implements DeleteCustomerByIdOutputPort {
+	
+	@Autowired
+	private CustomerRepository customerRepository;
+
+	@Override
+	public void delete(String id) {
+		customerRepository.deleteById(id);
+	}
+
+}
